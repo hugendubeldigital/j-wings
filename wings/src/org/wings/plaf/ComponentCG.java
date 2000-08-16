256448cdb45671921276a42e649921baf4cbfd78
mreinsch <mreinsch@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2000-08-16 14:42:53 +0000

    updated comments

--- wings/src/org/wings/plaf/ComponentCG.java
+++ wings/src/org/wings/plaf/ComponentCG.java
@@ -7,8 +7,23 @@ import org.wings.io.*;
 
 public interface ComponentCG
 {
+    /**
+     * Installs the CG. <p>
+     * <b>Note</b>: Be very carefull here as this method is called from
+     * the SComponent constructor! Don't call any methods which relay on
+     * something that will be constructed in a subconstructor later!
+     */
     void installCG(SComponent c);
+
+    /**
+     * Uninstalls the CG.
+     */
     void uninstallCG(SComponent c);
 
+    /**
+     * Writes the given component. <p>
+     * his method should be called from the write method in SComponent or
+     * a subclass.
+     */
     void write(Device d, SComponent c) throws IOException;
 }
