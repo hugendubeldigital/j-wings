656d773e288f59686a06d6ba2b11f4c9f5026686
hengels <hengels@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2000-09-01 09:53:40 +0000

    o changed the license to the GNU Lesser General Public License

--- wings/src/org/wings/Selectable.java
+++ wings/src/org/wings/Selectable.java
@@ -4,9 +4,9 @@
  *
  * This file is part of wingS (http://wings.mercatis.de).
  *
- * wingS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
  *
  * Please see COPYING for the complete licence.
