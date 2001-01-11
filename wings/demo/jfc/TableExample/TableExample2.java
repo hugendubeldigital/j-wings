8f5f7a317095a1ffaadb8a9e344a90eb1b834730
ahaaf <ahaaf@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2001-01-11 15:44:08 +0000

    initial

---
+++ wings/demo/jfc/TableExample/TableExample2.java
@@ -0,0 +1,63 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of the wingS demo (http://wings.mercatis.de).
+ *
+ * The wingS demo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
+package jfc.TableExample;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.wings.*;
+import org.wings.externalizer.*;
+import org.wings.servlet.*;
+import org.wings.session.*;
+
+/**
+ * TODO: documentation
+ *
+ * @author <a href="mailto:haaf@mercatis.de">Armin Haaf</a>
+ * @version $Revision$
+ */
+public class TableExample2
+    extends WingServlet
+{
+    protected void initExternalizer(ServletConfig config) {
+        // we want to use the servlet externalizer
+        getExternalizeManager().setExternalizer(new ServletExternalizer(config));
+    }
+
+    public SessionServlet generateSessionServlet(HttpServletRequest req)
+        throws Exception
+    {
+        // create new default session and set plaf
+        DefaultSession session = new DefaultSession();
+        session.getCGManager().setLookAndFeel(new org.wings.plaf.xhtml.css1.CSS1LookAndFeel());
+
+        // return a new TableExample2 session
+        return new TableExample2Session(session, req);
+    }
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+
+
+
+
+
