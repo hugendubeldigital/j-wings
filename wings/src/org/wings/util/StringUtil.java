7bfe4228217a4b64b3c44bd763e8c839ca7ac5a6
ahaaf <ahaaf@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2001-11-10 16:09:56 +0000

    counter to string

--- wings/src/org/wings/util/StringUtil.java
+++ wings/src/org/wings/util/StringUtil.java
@@ -90,6 +90,57 @@ public class StringUtil
 
         return erg;
     }
+
+  /**
+   * All possible chars for representing a number as a String
+   */
+  final static char[] digits = {
+    '0' , '1' , '2' , '3' , '4' , '5' ,
+    '6' , '7' , '8' , '9' , 'a' , 'b' ,
+    'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
+    'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
+    'o' , 'p' , 'q' , 'r' , 's' , 't' ,
+    'u' , 'v' , 'w' , 'x' , 'y' , 'z' ,
+    'A' , 'B' ,
+    'C' , 'D' , 'E' , 'F' , 'G' , 'H' ,
+    'I' , 'J' , 'K' , 'L' , 'M' , 'N' ,
+    'O' , 'P' , 'Q' , 'R' , 'S' , 'T' ,
+    'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'
+  };
+
+
+  /**
+   * Geht hoch bis Radix 62 !!
+   */
+  public static String toString(long i, int radix) {
+    char[] buf = new char[65];
+    int charPos = 64;
+    boolean negative = (i < 0);
+    
+    if (!negative) {
+      i = -i;
+    }
+    
+    while (i <= -radix) {
+      buf[charPos--] = digits[(int)(-(i % radix))];
+      i = i / radix;
+    }
+    buf[charPos] = digits[(int)(-i)];
+    
+    if (negative) {
+      buf[--charPos] = '-';
+    }
+    
+    return new String(buf, charPos, (65 - charPos));
+  }
+
+  /**
+   * 
+   */
+  public static String toShortestAlphaNumericString(long i) {
+    return toString(i, digits.length-1);
+  }
+
 }
 
 /*
