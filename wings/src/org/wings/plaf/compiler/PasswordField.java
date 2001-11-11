5894e5bcfdc9ddd41356b25be6fcd46537ae7931
mreinsch <mreinsch@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2001-11-11 14:22:25 +0000

    wrong ext

--- wings/src/org/wings/plaf/compiler/PasswordField.java
+++
@@ -1,8 +0,0 @@
-<template name="PasswordField" for="org.wings.SPasswordField">
-<%@ include file="common.plaf">
-<input type="password" size="<%?columns%>" maxlength="<%?maxColumns%>" \
-<% if(!component.isEditable()) { %>readonly="1" <%}%>\
-<% if(component.isEnabled())   { %>name="<%?namePrefix%>" <%}%>\
-<% printStyle(<%?style%>) %>\
-value="<%?text%>"/>
-</template>
