8375f799cd252cd3f3b660161758ea904e0231c3
ahaaf <ahaaf@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2001-11-13 13:31:20 +0000

    revised

--- wings/demo/frameset/FrameSetSession.java
+++ wings/demo/frameset/FrameSetSession.java
@@ -1,3 +1,17 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of wingS (http://wings.mercatis.de).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
 package frameset;
 
 import java.awt.event.*;
@@ -11,6 +25,13 @@ import org.wings.session.*;
 
 public class FrameSetSession
     extends SessionServlet{
+  
+    private int leftCount = 0;
+    private int rightCount = 0;
+
+    private SLabel leftLabel = createLabel("" + leftCount);
+    private SLabel rightLabel = createLabel("" + rightCount);
+  
 
 
     public FrameSetSession(Session session, HttpServletRequest req) {
@@ -20,51 +41,115 @@ public class FrameSetSession
     public void postInit(ServletConfig config)
         throws ServletException{
 
-        /*
-        hier startpanel bauen. ob man mit frames arbeitet,
-        oder getFrame().getContentPane().add(...) nutzt, ist hier egal.
+        getSession().setReloadManager(new FrameSetReloadManager());
 
-        */
+        SFrameSet vertical = new SFrameSet(new SFrameSetLayout(null, "50,*"));
+        SFrame toolbarFrame = new SFrame("toolbar");
+        toolbarFrame.getContentPane().setLayout(new SBorderLayout());
+        toolbarFrame.getContentPane().add(new TimestampLabel(), SBorderLayout.EAST);
+        vertical.add(toolbarFrame);
 
-        SButton btnShow = new SButton("Show the frames!");
-        btnShow.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event) {
-              showFrames();
-            }
-        });
+        SFrameSet horizontal = new SFrameSet(new SFrameSetLayout("210,*", null));
+        vertical.add(horizontal);
+        SFrame leftFrame = new SFrame("left frame");
+        leftFrame.getContentPane().setLayout(new SBorderLayout());
+        leftFrame.getContentPane().add(new TimestampLabel(), SBorderLayout.EAST);
+        horizontal.add(leftFrame);
+        SFrame rightFrame = new SFrame("right frame");
+        rightFrame.getContentPane().setLayout(new SBorderLayout());
+        rightFrame.getContentPane().add(new TimestampLabel(), SBorderLayout.EAST);
+        horizontal.add(rightFrame);
 
-        this.getFrame().getContentPane().add(btnShow);
 
+
+        ActionListener leftIncrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    leftCount++;
+                    leftLabel.setText("" + leftCount);
+                }
+            };
+        ActionListener leftDecrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    leftCount--;
+                    leftLabel.setText("" + leftCount);
+                }
+            };
+        ActionListener rightIncrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    rightCount++;
+                    rightLabel.setText("" + rightCount);
+                }
+            };
+        ActionListener rightDecrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    rightCount--;
+                    rightLabel.setText("" + rightCount);
+                }
+            };
+
+
+        SButton leftDecrementButton = createButton("decrement left");
+        leftDecrementButton.addActionListener(leftDecrement);
+        SButton leftIncrementButton = createButton("increment left");
+        leftIncrementButton.addActionListener(leftIncrement);
+
+        SButton rightDecrementButton = createButton("decrement right");
+        rightDecrementButton.addActionListener(rightDecrement);
+        SButton rightIncrementButton = createButton("increment right");
+        rightIncrementButton.addActionListener(rightIncrement);
+
+        SButton leftRightIncrementButton = createButton("increment left and right");
+        leftRightIncrementButton.addActionListener(leftIncrement);
+        leftRightIncrementButton.addActionListener(rightIncrement);
+
+        SPanel toolbarPanel = new SPanel(null);
+        toolbarPanel.add(leftIncrementButton);
+        toolbarPanel.add(rightIncrementButton);
+        toolbarPanel.add(leftRightIncrementButton);
+        toolbarFrame.getContentPane().add(toolbarPanel, SBorderLayout.CENTER);
+
+        SPanel leftFramePanel = new SPanel(null);
+        leftFramePanel.add(leftDecrementButton);
+        leftFramePanel.add(leftLabel);
+        leftFrame.getContentPane().add(leftFramePanel, SBorderLayout.CENTER);
+
+        SPanel rightFramePanel = new SPanel(null);
+        rightFramePanel.add(rightDecrementButton);
+        rightFramePanel.add(rightLabel);
+        rightFrame.getContentPane().add(rightFramePanel, SBorderLayout.CENTER);
+
+        setFrame(vertical);
     }
 
-    private void showFrames(){
-        getSession().setReloadManager(new FrameSetReloadManager());
-        SFrameSet newSet = new SFrameSet(new SFrameSetLayout(null, "30,*"));
+    SButton createButton(String text) {
+        SButton b = new SButton(text + "&nbsp;&nbsp;&nbsp;&nbsp;");
+        return b;
+    }
 
-	// * * * WORKAROUND * * *
-	String server = getFrame().getServerAddress().getAbsoluteAddress();
-	newSet.setServer(server);
-
-        this.setFrame(newSet);
-
-        SFrame menuFrame = new SFrame("menu frame");
-        newSet.add(menuFrame);
-        SFrame mainFrame = new SFrame("main frame");
-        newSet.add(mainFrame);
-
-        menuFrame.getContentPane().setLayout(null);
-
-        //mit label geht's
-        //menuFrame.getContentPane().add(new SLabel("MenuLabel"));
-
-        //mit button nicht
-        menuFrame.getContentPane().add(new SButton("MenuButton"));
-
-        mainFrame.getContentPane().setLayout(null);
-        mainFrame.getContentPane().add(new SLabel("Main"));
+    SLabel createLabel(String text) {
+        SLabel l = new SLabel(text);
+        l.setBorder(new SLineBorder());
+        return l;
     }
 
     public String getServletInfo() {
-        return "FrameSet";
+        return "FrameSet Demo ($Revision$)";
     }
 }
+
+class TimestampLabel extends SLabel {
+    TimestampLabel() {
+        setHorizontalAlignment(RIGHT);
+    }
+        
+    public String getText() {
+        return "rendered&nbsp;at&nbsp;" + System.currentTimeMillis();
+    }
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
