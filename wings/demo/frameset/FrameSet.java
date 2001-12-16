4ed66687955ee17005a766275e7aba6f8bb65c01
hengels <hengels@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2001-12-16 20:09:02 +0000

    *** empty log message ***

--- wings/demo/frameset/FrameSet.java
+++ wings/demo/frameset/FrameSet.java
@@ -1,13 +1,27 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of the wingS demo (http://wings.mercatis.de).
+ *
+ * The wingS demo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
 package frameset;
 
 import java.io.IOException;
-import java.net.URL;
+import java.net.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 import org.wings.*;
-import org.wings.externalizer.*;
+import org.wings.plaf.*;
 import org.wings.servlet.*;
 import org.wings.session.*;
 
@@ -20,20 +34,25 @@ import org.wings.session.*;
 public class FrameSet
     extends WingServlet
 {
-    protected void initExtObjectHandler(ServletConfig config) {
-        extManager.addObjectHandler(new ImageObjectHandler("gif"));
-        extManager.addObjectHandler(new ImageIconObjectHandler("gif"));
-        extManager.addObjectHandler(new ResourceImageIconObjectHandler());
-        extManager.addObjectHandler(new StyleSheetObjectHandler());
-        extManager.addObjectHandler(new TextObjectHandler("text/html", "html"));
-    }
-
     public SessionServlet generateSessionServlet(HttpServletRequest req)
         throws Exception
     {
+        // create new default session and set plaf
         DefaultSession session = new DefaultSession();
-        session.getCGManager().setLookAndFeel(new URL(new URL(HttpUtils.getRequestURL(req).toString()),
-                                                      "../css1.jar"));
+
+        if (!LookAndFeelFactory.isDeployed("xhtml/css1")) {
+            try {
+                URL url = servletConfig.getServletContext().getResource("css1.jar");
+                LookAndFeelFactory.deploy(url);
+            }
+            catch (Exception e) {
+                System.err.println(e.getMessage());
+                e.printStackTrace(System.err);
+            }
+        }
+        session.getCGManager().setLookAndFeel("xhtml/css1");
+
+        // return a new frameSet session
         return new FrameSetSession(session, req);
     }
 }
--- wings/demo/frameset/FrameSetSession.java
+++ wings/demo/frameset/FrameSetSession.java
@@ -1,70 +1,167 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of the wingS demo (http://wings.mercatis.de).
+ *
+ * The wingS demo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
 package frameset;
 
 import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.wings.util.*;
 import org.wings.*;
 import org.wings.servlet.*;
 import org.wings.session.*;
 
-public class FrameSetSession
-    extends SessionServlet{
+import org.wings.io.Device;
+import org.wings.io.ServletDevice;
 
+/**
+ * TODO: documentation
+ *
+ * @author Holger Engels
+ * @version $Revision$
+ */
+public class FrameSetSession
+    extends SessionServlet
+{
+    private SFrameSet vertical;
+    private SFrameSet horizontal;
+    private SFrame toolbarFrame;
+    private SFrame menuFrame;
+    private SFrame mainFrame;
+    private ReloadManagerFrame reloadManagerFrame;
+
+    private SButton menuButton = new SButton("menu frame");
+    private SButton mainButton = new SButton("main frame");
+
+    private int menuCount = 0;
+    private int mainCount = 0;
 
     public FrameSetSession(Session session, HttpServletRequest req) {
         super(session);
     }
 
     public void postInit(ServletConfig config)
-        throws ServletException{
+        throws ServletException
+    {
+        vertical = new SFrameSet(new SFrameSetLayout(null, "30,*"));
+        toolbarFrame = new SFrame("toolbar");
+        vertical.add(toolbarFrame);
 
-        /*
-        hier startpanel bauen. ob man mit frames arbeitet,
-        oder getFrame().getContentPane().add(...) nutzt, ist hier egal.
+        horizontal = new SFrameSet(new SFrameSetLayout("210,*", null));
+        vertical.add(horizontal);
+        menuFrame = new SFrame("menu frame");
+        horizontal.add(menuFrame);
+        mainFrame = new SFrame("main frame");
+        horizontal.add(mainFrame);
 
-        */
+        vertical.setBaseTarget("_top");
+        setFrame(vertical);
 
-        SButton btnShow = new SButton("Show the frames!");
-        btnShow.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event) {
-              showFrames();
-            }
-        });
-
-        this.getFrame().getContentPane().add(btnShow);
-
+        buildFrames(toolbarFrame, menuFrame, mainFrame);
     }
 
-    private void showFrames(){
-        getSession().setReloadManager(new FrameSetReloadManager());
-        SFrameSet newSet = new SFrameSet(new SFrameSetLayout(null, "30,*"));
+    protected void buildFrames(SFrame toolbar, SFrame menu, SFrame main) {
+        ActionListener menuIncrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    menuCount++;
+                    menuButton.setText("menu frame: " + menuCount);
+                }
+            };
+        ActionListener mainIncrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    mainCount++;
+                    mainButton.setText("main frame: " + mainCount);
+                }
+            };
 
-	// * * * WORKAROUND * * *
-	String server = getFrame().getServerAddress().getAbsoluteAddress();
-	newSet.setServer(server);
+        ActionListener menuDecrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    menuCount--;
+                    menuButton.setText("menu frame: " + menuCount);
+                }
+            };
+        ActionListener mainDecrement = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    mainCount--;
+                    mainButton.setText("main frame: " + mainCount);
+                }
+            };
 
-        this.setFrame(newSet);
+        SButton menuModifier = new SButton("reload menu frame&nbsp;&nbsp;&nbsp;&nbsp;");
+        menuModifier.addActionListener(menuIncrement);
 
-        SFrame menuFrame = new SFrame("menu frame");
-        newSet.add(menuFrame);
-        SFrame mainFrame = new SFrame("main frame");
-        newSet.add(mainFrame);
+        SButton mainModifier = new SButton("reload main frame&nbsp;&nbsp;&nbsp;&nbsp;");
+        mainModifier.addActionListener(mainIncrement);
 
-        menuFrame.getContentPane().setLayout(null);
+        SButton bothModifier = new SButton("reload both frames&nbsp;&nbsp;&nbsp;&nbsp;");
+        bothModifier.addActionListener(menuIncrement);
+        bothModifier.addActionListener(mainIncrement);
 
-        //mit label geht's
-        //menuFrame.getContentPane().add(new SLabel("MenuLabel"));
+        toolbar.getContentPane().add(menuModifier);
+        toolbar.getContentPane().add(mainModifier);
+        toolbar.getContentPane().add(bothModifier);
 
-        //mit button nicht
-        menuFrame.getContentPane().add(new SButton("MenuButton"));
+        menu.getContentPane().setLayout(null);
+        menu.getContentPane().add(menuButton);
 
-        mainFrame.getContentPane().setLayout(null);
-        mainFrame.getContentPane().add(new SLabel("Main"));
+        main.getContentPane().setLayout(null);
+        main.getContentPane().add(mainButton);
+
+        menuButton.addActionListener(menuDecrement);
+        mainButton.addActionListener(mainDecrement);
     }
 
+    void installReloadManagerFrame() {
+        // add reload manager frame
+        vertical.setLayout(new SFrameSetLayout(null, "30,*, 10"));
+        reloadManagerFrame = new ReloadManagerFrame();
+        vertical.add(reloadManagerFrame);
+
+        // set base target
+        toolbarFrame.setBaseTarget("frame" + reloadManagerFrame.getUnifiedId());
+        menuFrame.setBaseTarget("frame" + reloadManagerFrame.getUnifiedId());
+        mainFrame.setBaseTarget("frame" + reloadManagerFrame.getUnifiedId());
+
+        // set target resource
+        toolbarFrame.setBaseTarget(reloadManagerFrame.getUnifiedId());
+        menuFrame.setBaseTarget(reloadManagerFrame.getUnifiedId());
+        mainFrame.setBaseTarget(reloadManagerFrame.getUnifiedId());
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        Set dirtyResources = getSession().getReloadManager().getDirtyResources();
+        reloadManagerFrame.setDirtyResources(dirtyResources);
+	getSession().getReloadManager().clear();
+    }
+
+    /**
+     * Servletinfo
+     */
     public String getServletInfo() {
-        return "FrameSet";
+        return "FrameSet ($Revision$)";
     }
 }
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
