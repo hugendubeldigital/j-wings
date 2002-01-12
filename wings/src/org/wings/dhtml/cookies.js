2ea4ff9aeb724d3789f62dc86cb797379e2ecac0
alison <alison@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2002-01-12 22:22:08 +0000

    header added

--- wings/src/org/wings/dhtml/cookies.js
+++ wings/src/org/wings/dhtml/cookies.js
@@ -1,3 +1,18 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of wingS (http://wings.mercatis.de).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ * @author <a href="mailto:andre@lison.de">Andre Lison</a>
+ */
+
   function getCookie(name) { // use: getCookie("name");
     var re = new RegExp(name + "=([^;]+)");
     var value = re.exec(document.cookie);
--- wings/src/org/wings/dhtml/windows.js
+++ wings/src/org/wings/dhtml/windows.js
@@ -1,3 +1,18 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of wingS (http://wings.mercatis.de).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ * @author <a href="mailto:andre@lison.de">Andre Lison</a>
+ */
+
 function WindowManager() {
     this.count=0;
     this.windows = new Array();
