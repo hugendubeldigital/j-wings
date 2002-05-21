2e9b33aaa85bdcc1c013e236a75ec6121e468a2d
alison <alison@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2002-05-21 11:00:58 +0000

    Introducing SMenuPane with demo (ant menupane)

---
+++ wingx/src/org/wingx/plaf/MenuPaneCG.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of wingS (http://wings.mercatis.de).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
+package org.wingx.plaf;
+
+public interface MenuPaneCG extends org.wings.plaf.ContainerCG
+{
+
+}
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+
---
+++ wingx/src/org/wingx/plaf/xhtml/css1/MenuPaneCG.java
@@ -0,0 +1,34 @@
+/*
+ * $Id$
+ * (c) Copyright 2000 wingS development team.
+ *
+ * This file is part of wingS (http://wings.mercatis.de).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+
+package org.wingx.plaf.xhtml.css1;
+
+/**
+  * CG for {@link org.wingx.SMenuPane}
+  * @author <a href="mailto:andre@lison.de">Andre Lison</a>
+  * @version $Revision$
+  */
+public class MenuPaneCG extends org.wingx.plaf.xhtml.MenuPaneCG
+{
+
+    /**
+     * Constructor for MenuPaneCG.
+     */
+    public MenuPaneCG()
+    {
+        super();
+    }
+
+}
+
