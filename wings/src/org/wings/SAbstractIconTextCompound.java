2e621cf940dae846df238d77fdb2946b9a2233ef
ahaaf <ahaaf@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2002-10-17 16:25:29 +0000

    o isSelected must be overwritable (SButton returns always false...)

--- wings/src/org/wings/SAbstractIconTextCompound.java
+++ wings/src/org/wings/SAbstractIconTextCompound.java
@@ -424,7 +424,7 @@ public abstract class SAbstractIconTextCompound
      *
      * @return
      */
-    public final boolean isSelected() {
+    public boolean isSelected() {
         return selected;
     }
 
