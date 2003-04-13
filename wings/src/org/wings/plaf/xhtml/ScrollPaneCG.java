bdcc36cf6280f718e64402959e74fcba4f68c594
hzeller <hzeller@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2003-04-13 18:26:29 +0000

    o fix import line.

--- wings/src/org/wings/plaf/xhtml/ScrollPaneCG.java
+++ wings/src/org/wings/plaf/xhtml/ScrollPaneCG.java
@@ -17,7 +17,8 @@ package org.wings.plaf.xhtml;
 import java.awt.*;
 import java.io.IOException;
 
-import org.wings.*; import org.wings.border.*;
+import org.wings.*; 
+import org.wings.border.*;
 import org.wings.io.*;
 import org.wings.plaf.*;
 
