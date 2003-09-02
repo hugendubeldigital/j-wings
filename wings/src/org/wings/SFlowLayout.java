0491ac1fc7f2eeda209f0eb07ba735ff128bde4e
oliverscheck <oliverscheck@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2003-09-02 15:52:25 +0000

    cleaned up javadoc comments

--- wings/src/org/wings/SFlowLayout.java
+++ wings/src/org/wings/SFlowLayout.java
@@ -21,7 +21,7 @@ import org.wings.plaf.*;
 import org.wings.io.Device;
 
 /**
- * This is a flow layout
+ * The flow layout arranges components in a left-to-right or top-to-bottom order.
  *
  * @author <a href="mailto:haaf@mercatis.de">Armin Haaf</a>
  * @version $Revision$
@@ -35,22 +35,23 @@ public class SFlowLayout
     private static final String cgClassID = "FlowLayoutCG";
 
     /**
-     * TODO: documentation
+     * List of layouted components.
      */
     protected final List components;
 
     /**
-     * TODO: documentation
+     * Orientation (horizontally or vertically) of components. 
      */
     protected int orientation;
+
     /**
-     * TODO: documentation
+     * Alignment (left, center, right) of components.
      */
     protected int align;
 
     /**
-     * creates a new flow layout with horizontal orientation and
-     * left alignment
+     * Creates a new <code>SFlowLayout</code> with horizontal orientation and
+     * left alignment.
      */
     public SFlowLayout() {
         components = new ArrayList(2);
@@ -59,9 +60,7 @@ public class SFlowLayout
     }
 
     /**
-     *
-    /**
-     * creates a new flow layout with the given alignment
+     * Creates a new <code>SFlowLayout</code> with horizonal orientation and the given alignment.
      *
      * @param alignment the alignment
      */
@@ -70,6 +69,13 @@ public class SFlowLayout
         setAlignment(alignment);
     }
 
+    /**
+     * Adds the given component at given index. 
+     *
+     * @param c component to add
+     * @param constraint is ignored in this layout manager!
+     * @param index position to add component to
+     */
     public void addComponent(SComponent c, Object constraint, int index) {
         components.add(index, c);
     }
@@ -97,11 +103,13 @@ public class SFlowLayout
     }
 
     /**
-     * Sets the orientation. Use one of the following types:
+     * Sets the orientation for this layout. Possible values are
+     * <ul>
+     *   <li>{@link org.wings.SConstants#HORIZONTAL}
+     *   <li>{@link org.wings.SConstants#VERTICAL}
+     * </ul>
      *
-     * @param o One of the following constants:
-     *          {@link <code>SConstants#HORIZONTAL</code>} or
-     *          {@link <code>SConstants#VERTICAL</code>}
+     * @param o one of the orientation values shown above
      */
     public void setOrientation(int o) {
         orientation = o;
@@ -114,12 +122,14 @@ public class SFlowLayout
     public int getOrientation() { return orientation; }
 
     /**
-     * Sets the alignment. Use one of the following types:
+     * Sets the alignment for this layout. Possible values are
+     * <ul>
+     *   <li>{@link org.wings.SConstants#LEFT_ALIGN}
+     *   <li>{@link org.wings.SConstants#CENTER_ALIGN}
+     *   <li>{@link org.wings.SConstants#RIGHT_ALIGN}
+     * </ul>
      *
-     * @param a One of the following constants:
-     *          {@link <code>SConstants#LEFT_ALIGN</code>},
-     *          {@link <code>SConstants#CENTER_ALIGN</code>} or
-     *          {@link <code>SConstants#RIGHT_ALIGN</code>}
+     * @param a one of the allignment values shown above
      */
     public void setAlignment(int a) {
         align = a;
