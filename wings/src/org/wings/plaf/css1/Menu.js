ff43609209b11d9f9e490550b55b088d826173fa
arminhaaf <arminhaaf@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2004-02-27 12:59:33 +0000

    o konqueror support is slightly improved o code reduction o menu is marked, if selected

--- wings/src/org/wings/plaf/css1/Menu.js
+++ wings/src/org/wings/plaf/css1/Menu.js
@@ -21,6 +21,25 @@ function concatArrays(array1, array2) {
     return result;
 }
 
+String.prototype.endsWith = function(c) {
+    return this.startsWith(c, this.length - c.length);
+}
+
+String.prototype.startsWith = function(c) {
+    return this.startsWidth(c, 0);
+}
+
+String.prototype.startsWith = function(c, startIndex) {
+    for ( var i = 0; i<c.length; i++ ) {
+        if ( i+startIndex >= this.length ||
+             this.charAt(i+startIndex) != c.charAt(i) ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 var MENUS = new Array();
 
 function Menu(parent, hookElement, element) {
@@ -28,19 +47,17 @@ function Menu(parent, hookElement, element) {
     this.hookElement = hookElement;
     this.element = element;
     this.direction = (parent == null) ? 'vertical' : 'horizontal';
-    this.depth = 1;
+    this.element.style.position = 'absolute';
 
-    var node = this;
-	while (node.parent != null) {
-	    node = node.parent;
-	    this.depth = this.depth + 1;
-    }
+    // bei allen child hook elementen mouseOver und mouseOut setzen
+    this.setMouseMotion(element);
 }
+
 
 Menu.prototype.hideAll = function() {
     for (var id in MENUS) {
         var menu = MENUS[id];
-        menu.element.style.display = 'none';
+        setMenuVisible(menu.element, false);
     }
 }
 
@@ -50,13 +67,13 @@ Menu.prototype.printAll = function() {
         menuItem = MENUS[id];
         string = string + menuItem.hookElement.id + " ";
     }
-    alert(string);
+    //alert(string);
 }
 
 Menu.prototype.toggle = function(parentId, hookId, menuId) {
     var menuElement = document.getElementById(menuId);
 
-    if (menuElement.style.display == 'none') {
+    if (!isMenuVisible(menuElement)) {
         var parent = MENUS[parentId];
 
         var menu = MENUS[menuId];
@@ -70,7 +87,10 @@ Menu.prototype.toggle = function(parentId, hookId, menuId) {
     }
     else {
         var menu = MENUS[menuId];
-        menu.hide();
+
+        if ( menu ) {
+            menu.hide();
+        }
     }
 
     this.toggleFormElements();
@@ -78,21 +98,28 @@ Menu.prototype.toggle = function(parentId, hookId, menuId) {
 
 Menu.prototype.show = function() {
     this.applyPosition();
-    this.element.style.zIndex = this.depth;
-    this.element.style.display = 'inline';
+    setMenuVisible(this.element, true);
+    setStyleClass(this.hookElement, "smenu");
 
     for (var id in MENUS) {
         var menuItem = MENUS[id];
-        if (!this.isParent(menuItem))
-            menuItem.element.style.display = 'none';
+        if (!this.isParent(menuItem)) {
+            setStyleClass(menuItem.hookElement, "menu");
+            setMenuVisible(menuItem.element, false);
+        } else {
+            setStyleClass(menuItem.hookElement, "smenu");
+            setMenuVisible(menuItem.element, true);
+        }
     }
 }
 
 Menu.prototype.hide = function() {
+    setStyleClass(this.hookElement, "menu");
     for (var id in MENUS) {
         menuItem = MENUS[id];
-        if (this.isChild(menuItem))
-            menuItem.element.style.display = 'none';
+        if (this.isChild(menuItem)) {
+            setMenuVisible(menuItem.element, false);
+        }
     }
 }
 
@@ -129,18 +156,21 @@ Menu.prototype.applyPosition = function() {
         this.element.style.top = (bounds.y + bounds.height + 1) + "px";
         this.element.style.left = (bounds.x + 1) + "px";
         if ( ie5 ) {
-	    this.element.posX = bounds.x + 1;
-	    this.element.posY = bounds.y + bounds.height + 1;
+	        this.element.posX = bounds.x + 1;
+	        this.element.posY = bounds.y + bounds.height + 1;
         }
     }
     else if (this.direction == 'horizontal') {
         this.element.style.top = (bounds.y + 1) + "px";
         this.element.style.left = (bounds.x + bounds.width + 1) + "px";
         if ( ie5 ) {
-	    this.element.posX = bounds.x + bounds.width + 1;
-	    this.element.posY = bounds.y + 1;
+	        this.element.posX = bounds.x + bounds.width + 1;
+	        this.element.posY = bounds.y + 1;
         }
     }
+
+    //alert("set position " + this.element.style.left + " " + this.element.style.top + " bounds " +
+    //        bounds.x + ":" + bounds.y + ":" + bounds.width + ":" + bounds.height);
 }
 
 Menu.prototype.toggleFormElements = function() {
@@ -148,15 +178,71 @@ Menu.prototype.toggleFormElements = function() {
     var i = 0;
     for (var id in MENUS) {
         menuItem = MENUS[id];
-        if (menuItem.element.style.display != 'none')
+        if ( isMenuVisible(menuItem.element) )
             menuBounds[i++] = new Bounds(menuItem.element);
     }
     toggleFormElements(menuBounds);
 }
 
+
+
+
+Menu.prototype.setMouseMotion = function(element) {
+    if ( element.id && element.id.endsWith("hook") ) {
+        this.setMouseMotionStyles(element);
+    } else {
+        var kids = element.childNodes;
+        for (var i = 0; i < kids.length; i++) {
+            this.setMouseMotion(kids[i]);
+        }
+    }
+}
+
+Menu.prototype.setMouseMotionStyles = function(element) {
+    element.onmouseover = function(event) { Menu.prototype.setMouseOverStyle(element); };
+    element.onmouseout = function(event) { Menu.prototype.setMouseOutStyle(element); };
+}
+
+Menu.prototype.setMouseOutStyle = function(element) {
+    if ( element.className!="smenu" ) {
+        if ( element.className == "amenuitem" ) {
+            setStyleClass(element, 'menuitem');
+        } else {
+            setStyleClass(element, 'menu');
+        }
+    }
+}
+
+
+Menu.prototype.setMouseOverStyle = function(element) {
+    if ( element.className!="smenu" ) {
+        if ( element.className == "menuitem" ) {
+            setStyleClass(element, 'amenuitem');
+        } else {
+            setStyleClass(element, 'amenu');
+        }
+    }
+}
+
+
+
+function printChildNodes(element) {
+
+    var result = element.id;
+
+    var kids = element.childNodes;
+    for (var i = 0; i < kids.length; i++) {
+        result += ":" + printChildNodes(kids[i]);
+
+
+    }
+
+    return result;
+}
+
 function toggleFormElements(elementBounds) {
 
-//    var string = "";
+    //var string = "";
 
     var selects = document.getElementsByTagName('select');
 
@@ -169,58 +255,131 @@ function toggleFormElements(elementBounds) {
     }
 
     for ( var i=0; i<selects.length; i++ ) {
-	var select = selects[i];
+	    var select = selects[i];
         var selectBounds = new Bounds(select);
 
-//	var selectBoundsString = selectBounds.x + ":" + 
-//                                 selectBounds.y + ":" + selectBounds.width + ":" + 
-//                                 selectBounds.height;
+	    var selectBoundsString = selectBounds.x + ":" +
+                                 selectBounds.y + ":" + selectBounds.width + ":" +
+                                 selectBounds.height;
 
-        var hit = false;
+        var intersects = false;
         for (var b = 0; b < elementBounds.length; ++b) {
             var bounds = elementBounds[b];
-            var elemBoundsString = bounds.x + ":" + bounds.y + ":" + bounds.width + ":" + bounds.height;
+            //var elemBoundsString = bounds.x + ":" + bounds.y + ":" + bounds.width + ":" + bounds.height;
 
             if (selectBounds.intersect(bounds)) {
-                hit = true;
-//                string = string + elemBoundsString + " intersects " + selectBoundsString + " \n";		
-  		break;
+                intersects = true;
+                //string = string + elemBoundsString + " intersects " + selectBoundsString + " \n";
+  		        break;
             }
-//            else {
-//                string = string + elemBoundsString + " not intersects " + selectBoundsString + " \n";		
-//            }
+            else {
+                //string = string + elemBoundsString + " not intersects " + selectBoundsString + " \n";
+           }
         }
-        setVisible(select, hit);
+        setVisible(select, !intersects);
     }
-//    alert(string);
+    //alert(string);
+}
+
+function setMenuVisible(element, visible) {
+    if (visible) {
+        if ( konqueror ) {
+            element.style.display = '';
+         } else {
+            element.style.display = 'inline';
+         }
+    } else {
+        element.style.display= 'none';
+    }
+}
+
+function isMenuVisible(element) {
+    return element.style.display!= 'none';
 }
 
 function setVisible(element, visible) {
-    if (visible)
-        element.style.visibility = 'hidden';
-    else
+    if (visible) {
         element.style.visibility = 'visible';
+    } else {
+        element.style.visibility= 'hidden';
+    }
 }
 
 function isVisible(element) {
-    return element.style.visibility == 'visible';
+    return element.style.visibility!= 'hidden';
 }
 
+function getBoundsText(obj) {
+  if ( obj == null ) {
+    return "";
+  } else {
+    return obj.id + obj.offsetTop + ":" + obj.offsetLeft + ":" + obj.offsetWidth + ":" + obj.offsetHeight;
+  }
+}
 
 function Bounds(object) {
-    this.x = object.posX ? object.posX : 0;
-    this.y = object.posY ? object.posY : 0;
-    this.width = object.offsetWidth;
+
+    this.x = 0;
+    this.y = 0;
+    this.width = object.offsetWidth;/* + object.style.marginLeft + object.style.borderLeftWidth +
+                object.style.paddingLeft + object.style.paddingRight +
+                object.style.borderRightWidth + object.style.marginRight;*/
     this.height = object.offsetHeight;
 
-    // only calc if not a fixed position
-    if ( !object.posX ) {
-        while (object.offsetParent != null) {
-            this.x += object.offsetLeft;
+    if (object.offsetParent) {
+
+        // konqueror fix, a tr gives here 0 width and height, so use width and height of parent table
+        if ( konqueror ) {
+            var parent = object.offsetParent;
+            while ( this.width == 0 && parent ) {
+                this.width = parent.offsetWidth;
+                parent = parent.offsetParent;
+            }
+            parent = object.offsetParent;
+            while ( this.height == 0 && parent ) {
+                this.height = parent.offsetHeight;
+                parent = parent.offsetParent;
+            }
+        }
+        while (object ) {
+                /*alert("offset parent " + object.tagName + "\n" +
+                      " top " + object.offsetTop + "\n" +
+                      " left " + object.offsetLeft + "\n" +
+                      " width " + object.offsetWidth + "\n" +
+                      " height " + object.offsetHeight + "\n" +
+                      "");*/
             this.y += object.offsetTop;
+            this.x += object.offsetLeft;
+
+            if  ( object.style.position == "absolute" ) {
+                break;
+            }
             object = object.offsetParent;
         }
+    } else if (object.x && object.y) {
+        this.x = object.x;
+        this.y = object.y;
+    } else {
+       // alert("offset parent not defined " + object.tagName + " " + object.id);
     }
+
+
+    //alert("bounds " + object.id + " " + this.x + ":" + this.y + ":" + this.width + ":" + this.height);
+
+
+    //this.x = object.posX ? object.posX : 0;
+    //this.y = object.posY ? object.posY : 0;
+    //this.width = object.offsetWidth;
+    //this.height = object.offsetHeight;
+
+    // only calc if not a fixed position
+    //if ( !object.posX) {
+    //    while (object != null) {
+    //        this.x += object.offsetLeft;
+    //        this.y += object.offsetTop;
+    //        object = object.offsetParent;
+    //    }
+    //}
 }
 
 Bounds.prototype.intersect = function(other) {
@@ -235,7 +394,7 @@ Bounds.prototype.intersect = function(other) {
 }
 
 function setStyleClass(item, newClass) {
-    item.style.visibility = 'hidden';
+    setVisible(item, false);
     item.className = newClass;
-    item.style.visibility = 'visible';
+    setVisible(item, true);
 }
