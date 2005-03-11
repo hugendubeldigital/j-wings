172e78972a688e5d83de7edeece9196baa8a16ca
blueshift <blueshift@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-03-11 18:22:57 +0000

    o comment

--- wings2/src/java/org/wings/plaf/PrefixAndSuffixDelegate.java
+++ wings2/src/java/org/wings/plaf/PrefixAndSuffixDelegate.java
@@ -1,22 +1,28 @@
 /*
- * Created on 02.03.2005
+ * $Id$
+ * Copyright 2000,2005 wingS development team.
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-package org.wings.plaf;
-
-import java.io.IOException;
-import java.io.Serializable;
+ * This file is part of wingS (http://www.j-wings.org).
+ *
+ * wingS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */package org.wings.plaf;
 
 import org.wings.SComponent;
 import org.wings.io.Device;
 
+import java.io.IOException;
+import java.io.Serializable;
+
 /**
- * @author ole
+ * wingS renders a HTML code in front an behind every component (for alginment, colouring, font style, etc.).
+ * Implemntors of this interface do this for special browsers.
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * @author ole
  */
 public interface PrefixAndSuffixDelegate extends Serializable {
     public void writePrefix(Device device, SComponent component) throws IOException;
