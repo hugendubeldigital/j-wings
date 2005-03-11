33d2f01ede2bfa9a490c6fca82892351454b865c
blueshift <blueshift@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-03-11 11:39:18 +0000

    o demonstrate issue with background and font style

--- wings2/demo/wingset/src/java/wingset/DynamicLayoutExample.java
+++ wings2/demo/wingset/src/java/wingset/DynamicLayoutExample.java
@@ -47,29 +47,43 @@ public class DynamicLayoutExample extends WingSetPane {
     }
 
     private static void addDummyLabels(final SPanel panel, final int amount) {
-        final String[] texts = {"<html><span>[%] A very short component</span>",
-                                "<html><span>[%] A much longer, unbreakable label for wrapping demonstration</span>",
-                                "<html><span>[%] And again a short one</span>"};
+        final String[] texts = {"<html><span>[%] A very short component (TopLeft)</span>",
+                                "<html><span>[%] A much longer, unbreakable label for wrapping demo (Default)</span>",
+                                "<html><span>[%] And again a short one (RightBottom, red)</span>",
+                                "<html><span>[%] A 2-line <br/> label (CenterCenter, bold-italic)</span>"};
+        final SFont boldItalic = new SFont(null, SFont.BOLD + SFont.ITALIC, SFont.DEFAULT_SIZE);
         final SBorder greenLineBorder = new SLineBorder();
         greenLineBorder.setColor(Color.green);
 
         for (int i = 0; i < amount; i++) {
-            final SLabel label = new SLabel(texts[i%3].replace("%", Integer.toString((i + 1))));
+            final SLabel label = new SLabel(texts[i % 4].replace("%", Integer.toString((i + 1))));
             label.setBorder(greenLineBorder);
             panel.add(label);
             if (i % 3 == 0) {
                 label.setVerticalAlignment(TOP);
+                label.setHorizontalAlignment(LEFT);
             }
-            if (i % 3 == 1) {
-                label.setHorizontalAlignment(CENTER);
-                label.setVerticalAlignment(CENTER);
-            }
+            if (i % 3 == 1) ;
             if (i % 3 == 2) {
                 label.setForeground(Color.RED);
                 label.setHorizontalAlignment(RIGHT);
                 label.setVerticalAlignment(BOTTOM);
             }
+            if (i % 3 == 3) {
+                label.setHorizontalAlignment(CENTER);
+                label.setVerticalAlignment(CENTER);
+                label.setFont(boldItalic);
+            }
         }
+    }
+
+    private static SPanel createPanel(SLayoutManager layout, int amount) {
+        final SPanel panel = new SPanel(layout);
+        //panel.setBorder(new SLineBorder());
+        panel.setBackground(new Color(210, 210, 210));
+        addDummyLabels(panel, amount);
+
+        return panel;
     }
 
 
@@ -79,15 +93,11 @@ public class DynamicLayoutExample extends WingSetPane {
             SBoxLayout horizontalLayout = new SBoxLayout(SBoxLayout.HORIZONTAL);
             horizontalLayout.setCellPadding(10);
             horizontalLayout.setBorder(1);
-            SPanel horizontalPanel = new SPanel(horizontalLayout);
-            addDummyLabels(horizontalPanel, 6);
-            add(horizontalPanel);
+            add(createPanel(horizontalLayout, 5));
 
             add(new SLabel("Vertical vanilla box layout"));
             SBoxLayout verticalLayout = new SBoxLayout(SBoxLayout.VERTICAL);
-            SPanel verticalPanel = new SPanel(verticalLayout);
-            addDummyLabels(verticalPanel, 6);
-            add(verticalPanel);
+            add(createPanel(verticalLayout, 5));
         }
     }
 
@@ -95,42 +105,18 @@ public class DynamicLayoutExample extends WingSetPane {
         public FlowLayoutDemoPanel() {
             super(new SFlowDownLayout());
             add(new SLabel("Horizontal, centered FlowLayout"));
-            add(createPanel(new SFlowLayout(SFlowLayout.CENTER)));
+            add(createPanel(new SFlowLayout(SFlowLayout.CENTER), 4));
             add(new SLabel("Horizontal, left-aligned FlowLayout"));
-            add(createPanel(new SFlowLayout(SFlowLayout.LEFT)));
+            add(createPanel(new SFlowLayout(SFlowLayout.LEFT), 4));
             add(new SLabel("Horizontal, right-aligned FlowLayout"));
-            add(createPanel(new SFlowLayout(SFlowLayout.RIGHT)));
+            add(createPanel(new SFlowLayout(SFlowLayout.RIGHT), 4));
             add(new SLabel("<html>&nbsp;"));
             add(new SLabel("Vertical, centered FlowLayout"));
-            add(createPanel(new SFlowDownLayout(SConstants.CENTER)));
+            add(createPanel(new SFlowDownLayout(SConstants.CENTER), 4));
             add(new SLabel("Vertical, left-aligned FlowLayout"));
-            add(createPanel(new SFlowDownLayout()));
+            add(createPanel(new SFlowDownLayout(), 3));
             add(new SLabel("Vertical, right-aligned FlowLayout"));
-            add(createPanel(new SFlowDownLayout(SConstants.RIGHT)));
-        }
-
-        private SPanel createPanel(SLayoutManager layout) {
-            final SPanel panel = new SPanel(layout);
-            panel.setBorder(new SLineBorder());
-            panel.setBackground(Color.LIGHT_GRAY);
-            addDummyLabels(panel, 3);
-
-            return panel;
-        }
-
-        private void addDummyLabels1(final SPanel panel) {
-            final SLabel label1 = new SLabel("<html><span>A very short component</span>");
-            final SLabel label2 = new SLabel("<html><span>A much longer, unbreakable label for wrapping demonstration</span>");
-            final SLabel label3 = new SLabel("<html><span>And again a short one</span>");
-            SBorder labelBorder = new SLineBorder();
-            labelBorder.setColor(Color.red);
-            label1.setBorder(labelBorder);
-            label2.setBorder(labelBorder);
-            label3.setBorder(labelBorder);
-
-            panel.add(label1);
-            panel.add(label2);
-            panel.add(label3);
+            add(createPanel(new SFlowDownLayout(SConstants.RIGHT), 4));
         }
     }
 
@@ -141,9 +127,8 @@ public class DynamicLayoutExample extends WingSetPane {
             layout1.setBorder(1);
             layout1.setCellPadding(10);
             layout1.setCellSpacing(20);
-            final SPanel panel1 = new SPanel(layout1);
-            addDummyLabels(panel1, 6);
-            add(panel1);
+
+            add(createPanel(layout1, 12));
         }
     }
 
