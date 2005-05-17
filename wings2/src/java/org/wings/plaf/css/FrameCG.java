bb200ad7f2340667cd18191b5bf9e3868032ce48
neurolabs <neurolabs@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-05-17 15:09:10 +0000

    * took out unnecessary logging

--- wings2/src/java/org/wings/plaf/css/FrameCG.java
+++ wings2/src/java/org/wings/plaf/css/FrameCG.java
@@ -313,7 +313,6 @@ public class FrameCG implements SConstants, org.wings.plaf.FrameCG {
             device.print(behaviorUrl);
             device.print(");}\n");
             device.print("</style>\n");
-            log.info("behaviorUrl: " + behaviorUrl);
         }
         
 
