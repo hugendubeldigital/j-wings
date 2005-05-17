97730b7b0a1f63e7fbc67513494f0160523cd29e
neurolabs <neurolabs@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-05-17 15:08:11 +0000

    * just code formatting

--- wings2/src/java/org/wings/plaf/css/CheckBoxCG.java
+++ wings2/src/java/org/wings/plaf/css/CheckBoxCG.java
@@ -19,8 +19,9 @@ import org.wings.io.Device;
 
 import java.io.IOException;
 
-public class CheckBoxCG        extends ButtonCG        implements SConstants, org.wings.plaf.CheckBoxCG {
-    private boolean useIconsInForm = false;
+public class CheckBoxCG extends ButtonCG implements SConstants,
+        org.wings.plaf.CheckBoxCG {
+    protected boolean useIconsInForm = false;
 
     public boolean isUseIconsInForm() {
         return useIconsInForm;
