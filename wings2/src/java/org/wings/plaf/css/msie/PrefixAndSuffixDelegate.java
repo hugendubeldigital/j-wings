0a09548725c2abff27840adc7bdbd035f2589d79
blueshift <blueshift@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-05-20 19:01:59 +0000

    o correct component alignment o reuse methods of AbstractLayoutCG o stretch surrounding table on right/center align for correct behaviour

--- wings2/src/java/org/wings/plaf/css/msie/PrefixAndSuffixDelegate.java
+++ wings2/src/java/org/wings/plaf/css/msie/PrefixAndSuffixDelegate.java
@@ -15,13 +15,10 @@ package org.wings.plaf.css.msie;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wings.LowLevelEventListener;
-import org.wings.SComponent;
-import org.wings.SDimension;
-import org.wings.SPopupMenu;
+import org.wings.*;
 import org.wings.border.STitledBorder;
 import org.wings.io.Device;
-import org.wings.plaf.ComponentCG;
+import org.wings.plaf.css.AbstractLayoutCG;
 import org.wings.plaf.css.InputMapScriptListener;
 import org.wings.plaf.css.Utils;
 import org.wings.plaf.css.VersionedInputMap;
@@ -35,17 +32,24 @@ import java.io.IOException;
 public class PrefixAndSuffixDelegate implements org.wings.plaf.PrefixAndSuffixDelegate {
     private final static transient Log log = LogFactory.getLog(PrefixAndSuffixDelegate.class);
 
-    public PrefixAndSuffixDelegate() { }
-    
+    public PrefixAndSuffixDelegate() {
+    }
+
     public void writePrefix(Device device, SComponent component) throws IOException {
         SDimension prefSize = component.getPreferredSize();
+        final int align = component.getHorizontalAlignment();
+
+        // For centering or right-alignment we need we surrounding helper table to be stretched to full width.
+        if (prefSize == null && (align == SConstants.CENTER || align == SConstants.RIGHT))
+            prefSize = new SDimension("100%", null);
+
+        StringBuffer inlineStyles = Utils.generateCSSInlinePreferredSize(prefSize);
+
         Utils.printDebugNewline(device, component);
         Utils.printDebug(device, "<!-- ").print(component.getName()).print(" -->");
-        device.print("<table id=\"");
-        device.print(component.getName()).print("\"");
-        Utils.optAttribute(device, "class", component.getStyle());
-        Utils.printCSSInlinePreferredSize(device, prefSize);
-            
+        device.print("<table id=\"").print(component.getName()).print("\"");
+        Utils.optAttribute(device, "class", "SLayout " + component.getStyle());
+        Utils.optAttribute(device, "style", inlineStyles.toString());
 
         if (component instanceof LowLevelEventListener) {
             LowLevelEventListener lowLevelEventListener = (LowLevelEventListener) component;
@@ -78,10 +82,10 @@ public class PrefixAndSuffixDelegate implements org.wings.plaf.PrefixAndSuffixDe
 
         SPopupMenu menu = component.getComponentPopupMenu();
         if (menu != null) {
-            ComponentCG menuCG = menu.getCG();
+            //ComponentCG menuCG = menu.getCG();
             String componentId = menu.getName();
             String popupId = componentId + "_pop";
-            String hookId = component.getName();
+            //String hookId = component.getName();
             device.print(" onContextMenu=\"javascript:return wpm_menuPopup(event, '");
             device.print(popupId);
             device.print("');\" onMouseDown=\"javascript:return wpm_menuPopup(event, '");
@@ -89,7 +93,9 @@ public class PrefixAndSuffixDelegate implements org.wings.plaf.PrefixAndSuffixDe
             device.print("');\"");
         }
 
-        device.print("><tr><td valign=\"top\">"); // table
+        device.print("><tr>"); // table
+        AbstractLayoutCG.openLayouterCell(device, false, 0, component);
+        device.print(">");
 
         // Special handling: Render title of STitledBorder
         if (component.getBorder() instanceof STitledBorder) {
@@ -106,7 +112,8 @@ public class PrefixAndSuffixDelegate implements org.wings.plaf.PrefixAndSuffixDe
 
     public void writeSuffix(Device device, SComponent component) throws IOException {
         component.fireRenderEvent(SComponent.DONE_RENDERING);
-        device.print("</td></tr></table>");
+        AbstractLayoutCG.closeLayouterCell(device, false);
+        device.print("</tr></table>");
         Utils.printDebug(device, "<!-- /").print(component.getName()).print(" -->");
     }
 
