014e8ab6aea04dc83138995dd944f866902a6214
neurolabs <neurolabs@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2005-07-08 14:59:36 +0000

    * bee puzzle drag and drop example * christian humers fix doesn't work...reverting

--- wings2/demo/wingset/src/java/wingset/DragAndDropExample.java
+++ wings2/demo/wingset/src/java/wingset/DragAndDropExample.java
@@ -13,21 +13,29 @@
  */
 package wingset;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wings.SBoxLayout;
+import org.wings.SButton;
 import org.wings.SComponent;
+import org.wings.SDimension;
+import org.wings.SGridLayout;
+import org.wings.SIcon;
 import org.wings.SLabel;
 import org.wings.SPanel;
 import org.wings.STextComponent;
 import org.wings.STextField;
+import org.wings.SURLIcon;
 import org.wings.dnd.DragSource;
 import org.wings.dnd.DropTarget;
 import org.wings.event.SComponentDropListener;
 import org.wings.session.SessionManager;
+import org.wings.style.CSSProperty;
 
 /**
  * example for showing the drag and drop capabilities of wingS
@@ -36,53 +44,148 @@ import org.wings.session.SessionManager;
  */
 public class DragAndDropExample extends WingSetPane {
     private final static Log log = LogFactory.getLog(DragAndDropExample.class);
-    private final SDndTextField dragSource = new SDndTextField("This is the DragSource.");
-    private final SDndTextField dropTarget = new SDndTextField("This is the DropTarget.");
-    private final SDndTextField normalField = new SDndTextField("This is a normal Label.");
-    private final SDragLabel dragLabel = new SDragLabel("This is a draggable Label.");
-    
+
+    private static final SURLIcon ICON_BEE1 = new SURLIcon("../icons/bee_1.jpg");
+    private static final SURLIcon ICON_BEE2 = new SURLIcon("../icons/bee_2.jpg");
+    private static final SURLIcon ICON_BEE3 = new SURLIcon("../icons/bee_3.jpg");
+    private static final SURLIcon ICON_BEE4 = new SURLIcon("../icons/bee_4.jpg");
+    private static final SURLIcon ICON_BEE5 = new SURLIcon("../icons/bee_5.jpg");
+    private static final SURLIcon ICON_BEE6 = new SURLIcon("../icons/bee_6.jpg");
+    private static final SURLIcon ICON_BEE7 = new SURLIcon("../icons/bee_7.jpg");
+    private static final SURLIcon ICON_BEE8 = new SURLIcon("../icons/bee_8.jpg");
+    private static final SURLIcon ICON_BEE9 = new SURLIcon("../icons/bee_9.jpg");
+
+    private final SDragLabel dragIconOne = new SDragLabel();
+    private final SDragLabel dragIconTwo = new SDragLabel();
+    private final SDragLabel dragIconThree = new SDragLabel();
+    private final SDragLabel dragIconFour = new SDragLabel();
+    private final SDragLabel dragIconFive = new SDragLabel();
+    private final SDragLabel dragIconSix = new SDragLabel();
+    private final SDragLabel dragIconSeven = new SDragLabel();
+    private final SDragLabel dragIconEight = new SDragLabel();
+    private final SDragLabel dragIconNine = new SDragLabel();
+
+    private final SDropLabel dropIconOne = new SDropLabel();
+    private final SDropLabel dropIconTwo = new SDropLabel();
+    private final SDropLabel dropIconThree = new SDropLabel();
+    private final SDropLabel dropIconFour = new SDropLabel();
+    private final SDropLabel dropIconFive = new SDropLabel();
+    private final SDropLabel dropIconSix = new SDropLabel();
+    private final SDropLabel dropIconSeven = new SDropLabel();
+    private final SDropLabel dropIconEight = new SDropLabel();
+    private final SDropLabel dropIconNine = new SDropLabel();
+
+    private final SIcon[] beeIcons = new SIcon[] {ICON_BEE1, ICON_BEE2, ICON_BEE3, ICON_BEE4, ICON_BEE5, ICON_BEE6, ICON_BEE7, ICON_BEE8, ICON_BEE9};
+    private final SDragLabel[] dragIcons = new SDragLabel[] {dragIconOne, dragIconTwo, dragIconThree, dragIconFour, dragIconFive, dragIconSix, dragIconSeven, dragIconEight, dragIconNine};
+    private final SDropLabel[] dropIcons = new SDropLabel[] {dropIconOne, dropIconTwo, dropIconThree, dropIconFour, dropIconFive, dropIconSix, dropIconSeven, dropIconEight, dropIconNine};
+
+    private int piecesRight;
+    private final SLabel statusLabel = new SLabel();
+
     protected SComponent createExample() {
         final SPanel container = new SPanel();
-        container.setLayout(new SBoxLayout(SBoxLayout.VERTICAL));
-        // initialize the drag and drop components
-        dragSource.setDragEnabled(true);
-        dragLabel.setDragEnabled(true);
-        // for dropTargets, adding a Listener is enough
-        dropTarget.addComponentDropListener(new SComponentDropListener() {
-
-            public boolean handleDrop(SComponent dragSource) {
-                boolean result = false;
-                if (dragSource instanceof STextComponent) {
-                    // allow drop
-                    dropTarget.setText(((STextComponent)dragSource).getText());
-                    result = true;
-                }
-                if (dragSource instanceof SLabel) {
-                    // allow drop
-                    dropTarget.setText(((SLabel)dragSource).getText());
-                    result = true;
-                }
-                container.add(new SLabel("Drag and Drop Action triggered from " + dragSource.getClass().getName()));
-                return result;
+        final SPanel puzzleContainer = new SPanel(new SBoxLayout(SBoxLayout.VERTICAL));
+        final SPanel controlContainer = new SPanel(new SBoxLayout(SBoxLayout.VERTICAL));
+        container.setLayout(new SBoxLayout(SBoxLayout.HORIZONTAL));
+        // control components
+        final SButton resetButton = new SButton("Reset");
+        resetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                resetPuzzle();
             }
-            
         });
-        // add the components to the container
-        container.add(dragSource);
-        container.add(normalField);
-        container.add(dropTarget);
-        container.add(dragLabel);
+        // initialize the drag components
+        for (int i = 0; i < dragIcons.length; i++) {
+            dragIcons[i].setDragEnabled(true);
+            dragIcons[i].setPreferredSize(new SDimension(30,30));
+        }
+        // initialize the drop components
+        for (int i = 0; i < dropIcons.length; i++) {
+            final int position = i;
+            final SDropLabel dropIcon = dropIcons[i];
+            dropIcon.setPreferredSize(new SDimension(100,100));
+            dropIcon.addComponentDropListener(new SComponentDropListener() {
+
+                // the drag and drop magic
+                public boolean handleDrop(SComponent dragSource) {
+                    if (dragIcons[position].equals(dragSource)) {
+                        dragIcons[position].setIcon(null);
+                        beeIcons[position].setIconHeight(100);
+                        beeIcons[position].setIconWidth(100);
+                        dropIcon.setIcon(beeIcons[position]);
+                        piecesRight++;
+                        if (piecesRight == 9) {
+                            statusLabel.setText("Look, it's got wingS ;) !");
+                            statusLabel.setAttribute(CSSProperty.FONT_WEIGHT, "600");
+                            statusLabel.setAttribute(CSSProperty.COLOR, "red");
+                        } else {
+                            statusLabel.setText("You have " + piecesRight + " pieces right! Can you guess what it is?");
+                        }
+                        return true;
+                    }
+                    statusLabel.setText("That piece doesn't belong there! What are you thinking?");
+                    return false;
+                }
+                
+            });
+        }
+        resetPuzzle();
+        
+        // build the puzzle
+        final SPanel puzzle = new SPanel();
+        final SGridLayout gridLayout = new SGridLayout(3,3);
+        gridLayout.setBorder(1);
+        puzzle.setLayout(gridLayout);
+        for (int i = 0; i < dropIcons.length; i++) {
+            puzzle.add(dropIcons[i]);
+        }
+        
+        // build the pieces area
+        final SPanel pieces = new SPanel();
+        final SBoxLayout boxLayout = new SBoxLayout(SBoxLayout.HORIZONTAL);
+        boxLayout.setBorder(1);
+        pieces.setLayout(boxLayout);
+        for (int i = 0; i < dragIcons.length; i++) {
+            pieces.add(dragIcons[i]);
+        }
+
+        puzzleContainer.add(puzzle);
+        puzzleContainer.add(pieces);
+        
+        controlContainer.add(statusLabel);
+        controlContainer.add(resetButton);
+        
+        container.add(puzzleContainer);
+        container.add(controlContainer);
         
         return container;
+    }
+
+    protected void resetPuzzle() {
+        piecesRight = 0;
+        statusLabel.setText("Try to solve the puzzle.");
+        statusLabel.setAttribute(CSSProperty.FONT_WEIGHT, "normal");
+        statusLabel.setAttribute(CSSProperty.COLOR, "black");
+        // init the icons
+        for (int i = 0; i < beeIcons.length; i++) {
+            beeIcons[i].setIconHeight(30);
+            beeIcons[i].setIconWidth(30);
+        }
+        // initialize the drag components
+        for (int i = 0; i < dragIcons.length; i++) {
+            SDragLabel dragIcon = dragIcons[i];
+            dragIcon.setIcon(beeIcons[i]);
+            dragIcon.setDragEnabled(true);
+        }
+        // initialize the drop components
+        for (int i = 0; i < dropIcons.length; i++) {
+            dropIcons[i].setIcon(null);
+        }
     }
 
     private class SDragLabel extends SLabel implements DragSource {
 
         private boolean dragEnabled;
-
-        public SDragLabel(String string) {
-            super(string);
-        }
 
         public boolean isDragEnabled() {
             return dragEnabled;
@@ -105,37 +208,8 @@ public class DragAndDropExample extends WingSetPane {
      * @author ole
      *
      */
-    private class SDndTextField extends STextField implements DragSource, DropTarget {
-        private boolean dragEnabled;
+    private class SDropLabel extends SLabel implements DropTarget {
         private ArrayList componentDropListeners = new ArrayList();
-
-        public SDndTextField() {
-            this("");
-        }
-
-        public SDndTextField(String string) {
-            super(string);
-            this.setColumns(40);
-        }
-
-        /* (non-Javadoc)
-         * @see org.wings.dnd.DragSource#isDragEnabled()
-         */
-        public boolean isDragEnabled() {
-            return dragEnabled;
-        }
-
-        /* (non-Javadoc)
-         * @see org.wings.dnd.DragSource#setDragEnabled(boolean)
-         */
-        public void setDragEnabled(boolean dragEnabled) {
-            this.dragEnabled = dragEnabled;
-            if (dragEnabled) {
-                SessionManager.getSession().getDragAndDropManager().registerDragSource(this);
-            } else {
-                SessionManager.getSession().getDragAndDropManager().deregisterDragSource(this);
-            }
-        }
 
         /* (non-Javadoc)
          * @see org.wings.dnd.DropTarget#addComponentDropListener(org.wings.event.SComponentDropListener)
